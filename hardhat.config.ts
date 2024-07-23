51c7bc4b6acc6b53b1973a603e2747db8306e2c6
adam <adam@avicenne.studio>
2024-07-23 12:20:11 +0200

    :wrench: add env file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,9 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
 };
