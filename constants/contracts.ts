c1eaed079e1a3669b2ed7d90999640835662bbc9
wepe <adam59140@gmail.com>
2024-12-09 21:02:52 +0400

    :wrench: reduce initial supply

--- constants/contracts.ts
+++ constants/contracts.ts
@@ -1,5 +1,5 @@
 // Token
-export const INITIAL_SUPPLY = (1_000_000_000n / 2n) * 10n ** 18n; // 500M = 50% of MAX_SUPPLY
+export const INITIAL_SUPPLY = 100_000_000n * 10n ** 18n; // 100M = 10% of MAX_SUPPLY
 export const TREASURY_WALLET_ADDRESS = process.env.TREASURY_WALLET_ADDRESS || "";
 export const FEE = 500n; // 5%
 
